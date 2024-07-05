ac7984d814044880695f8accd24929f9b86198e9
JoySarkarBD <developer.joysarkar@gmail.com>
2024-07-05 12:11:32 +0600

    resolve: button disable

--- src/app/[locale]/(Pages)/login/page.tsx
+++ src/app/[locale]/(Pages)/login/page.tsx
@@ -147,7 +147,7 @@ export default function Page() {
               type='submit'
               disabled={submittingState}
               className={`w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium ${
-                setSubmittingState
+                submittingState
                   ? "text-gray-300 bg-blue-900"
                   : "text-white bg-blue-600 hover:bg-blue-700"
               } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}>
